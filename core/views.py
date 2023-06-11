d499b0e0f65aa94a575ad0cd633548354dea2fb3
Rivado-E <redah@umd.edu>
2023-06-11 12:25:07 -0400

    first go at frontend

--- core/views.py
+++ core/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from datetime import date
+import calendar
 
-# Create your views here.
+
+#MACROS
+MONTHS = ['January', 'February', 'March', 'April', 'May', 'June',
+          'July', 'August', 'September', 'October', 'November', 'December']
+DAYS = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+
+#Views
+def displayMainpage(request):
+    today = date.today()
+    month = MONTHS[today.month - 1]
+    year = year = str(today.year)
+    return render(request, 'index.html', {'mm_yy':month+" "+year})
